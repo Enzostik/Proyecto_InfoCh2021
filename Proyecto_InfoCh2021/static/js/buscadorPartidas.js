78d386c1d55ce7ff487d8a9924c3fd472c58890c
IgnacioAlme <ignacio.yair.almenar@gmail.com>
2021-08-23 19:27:01 -0300

    Arreglado bug búsqueda de partidas

--- Proyecto_InfoCh2021/static/js/buscadorPartidas.js
+++ Proyecto_InfoCh2021/static/js/buscadorPartidas.js
@@ -3,5 +3,18 @@ function updateUrl() {
     const orden = document.getElementById('order').value;
     const fecha = document.getElementById('date').value;
 
-    window.location.href = window.location.href + `?search=${usuario}&ord=${orden}&met=${fecha}`;
+    var url = window.location.href;
+    var searchParams = url.searchParams;
+
+    if (searchParams !== null) {
+        console.log('NULL');
+        window.location.href = window.location.href + `?search=${usuario}&ord=${orden}&met=${fecha}`;
+    } else {
+        searchParams.set('search', usuario);
+        searchParams.set('ord', orden);
+        searchParams.set('met', fecha);
+
+        var new_url = url.toString();
+        window.location.href = new_url;
+    }
 }

a3094b087fc65703361b97f718490a50aef14d1c
Enzostik <enzoalmenar13@gmail.com>
2021-08-25 16:28:33 -0300

    Cambios en como se cargan las imágenes a la base de datos

--- Proyecto_InfoCh2021/static/js/profile.js
+++ Proyecto_InfoCh2021/static/js/profile.js
@@ -6,28 +6,28 @@ $(document).ready(function() {
       dato = $('.mi-imagen[data-id="1"]').attr("src");
       //alert(dato);
       $('#imagen').attr('src', dato);    
-      $('#image').val(dato);
+      $('#image').val('static/img/user.png');
     });
 
     $("#imagen2").on("click", function () {
       let dato = $('.mi-imagen[data-id="2"]').attr("src");
       //alert(dato);
       $('#imagen').attr('src', dato);    
-      $('#image').val(dato);
+      $('#image').val('static/img/moto.png');
     });
 
     $("#imagen3").on("click", function () {
       let dato = $('.mi-imagen[data-id="3"]').attr("src");
       //alert(dato);
       $('#imagen').attr('src', dato);    
-      $('#image').val(dato);
+      $('#image').val('static/img/auto.png');
     });
 
     $("#imagen4").on("click", function () {
       let dato = $('.mi-imagen[data-id="4"]').attr("src");
       //alert(dato);
       $('#imagen').attr('src', dato);    
-      $('#image').val(dato);
+      $('#image').val('static/img/pic/profile1.png');
     });
 });
   
